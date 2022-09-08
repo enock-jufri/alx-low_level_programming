b08676bf428468bd3069dbe937306bc8a22f28a8
enock-jufri <enockjufri@gmail.com>
2022-09-08 14:00:16 -0700

    updage

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	long int li;
 	long long int lli;
 
-	printf("size of a char: %libyte(s)",sizeof(c));
-	printf("size of an int: %ibyte(s)",sizeof(i));
-	printf("size of an int: %fbyte(s)",sizeof(f));
-	printf("size of an int: %libyte(s)",sizeof(li));
-	printf("size of an int: %llibyte(s)",sizeof(lli));
+	printf("size of a char: %libyte(s)\n", sizeof(c));
+	printf("size of an int: %ibyte(s)\n", sizeof(i));
+	printf("size of an int: %fbyte(s)\n", sizeof(f));
+	printf("size of an int: %libyte(s)\n", sizeof(li));
+	printf("size of an int: %llibyte(s)\n", sizeof(lli));
 	
 	return (0);
 }	
