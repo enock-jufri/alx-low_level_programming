3a500d68bfd3b8d2cc37b82537459aaee6e93b13
enock-jufri <enockjufri@gmail.com>
2022-09-09 11:13:42 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch, l[26];
+
+	for (ch = a;ch <= z; ch++)
+		l = putchar(ch);
+	putchar(l);
+	return (0);
+}
