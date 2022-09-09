452b2f5157c4a6c813806ab21d6c9d74c74ab35a
enock-jufri <enockjufri@gmail.com>
2022-09-09 14:08:08 -0700

    update

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar(n + '0');
+		putchar(',');
+		putchar(' ');
+	putchar('$');
+	return (0);
+}
