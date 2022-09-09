6bed80177d0425518bb172da09f8eed4b4e74d42
enock-jufri <enockjufri@gmail.com>
2022-09-09 13:24:43 -0700

    update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{	
+	char ch;
+
+	for (ch = 0; ch <= 9; ch++)
+		putchar(ch + '0');
+	putchar('\n');
+	return (0);
+}
