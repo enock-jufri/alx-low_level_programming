0e96dad08166be648527a87913caa92089804948
enock-jufri <enockjufri@gmail.com>
2022-09-13 11:56:43 -0700

    update

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,17 +2,19 @@
 /**
  * _islower - print lowercase
  *
- * Return: 0
+ * @c: The character in ASCII code
+ *
+ * Return: 1 for lowercase else 0
  */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }
 
