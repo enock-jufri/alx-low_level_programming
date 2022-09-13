eca647969531a072a7c9e66b416f87e834de97c9
enock-jufri <enockjufri@gmail.com>
2022-09-13 10:51:57 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
  * main - entry point
  *
  * Return: 0
  */
+
 int main(void)
 {
 	printf("_putchar\n");
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,6 +10,6 @@ void print_alphabet(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 
-		putchar(ch);
-	putchar('\n');
+		_putchar(ch);
+	_putchar('\n');
 }
