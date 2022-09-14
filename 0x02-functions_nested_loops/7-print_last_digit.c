5b96336c20255c5323881caf2030342fa9083ede
enock-jufri <enockjufri@gmail.com>
2022-09-14 01:33:42 -0700

    update

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main"
+/**
+ * print_last_digit - prints last digit of a number
+ *
+ * @n: input from main
+ *
+ * Return: 0
+ */
+int print_last_digit(int n)
+{
+	n=n%10;
+	return (0);
+}
