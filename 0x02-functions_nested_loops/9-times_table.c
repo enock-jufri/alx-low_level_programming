c20b0da0061a50190a9af5d6e08e9b67416ddfaf
enock-jufri <enockjufri@gmail.com>
2022-09-14 13:51:36 -0700

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,14 +11,13 @@ void times_table(void)
 		for (a = 0; a <= 9; a++)
 		{
 			_putchar(48);
-			for (b = 1;b <= 9; b++)
+			for (b = 1; b <= 9; b++)
 			{
 				c = a * b;
 				_putchar(44);
 				_putchar(32);
 				if (c <= 9)
 				{
-					_putchar(44);
 					_putchar(c + 48);
 				}
 				else
