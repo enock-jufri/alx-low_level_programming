d3cd6d3289b3a691c7d455295a8ab1db919860cf
enock-jufri <enockjufri@gmail.com>
2022-09-14 04:57:29 -0700

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,9 +5,19 @@
  * @n: input from main
  *
  * Return: 0
- */
-int print_last_digit(int n)
-{
-	n=n%10;
-	return (0);
-}
+ */int print_last_digit(int n)
+ {
+   int l;
+
+   l = n % 10;
+   if (l < 0)
+     {
+       _putchar(-l + 48);
+       return (-l);
+     }
+   else
+     {
+       _putchar(l + 48);
+       return (l);
+     }
+ }
