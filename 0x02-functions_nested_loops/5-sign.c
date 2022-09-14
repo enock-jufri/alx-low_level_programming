0e5fbe797091b296ae095fb48e75ebb8c9deaee5
enock-jufri <enockjufri@gmail.com>
2022-09-14 00:24:11 -0700

    update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,34 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - entry point
+ * print_sign - returns 1 if input is > 1, 0 if 0 and < 1 -1
  *
- *Return: 0
+ * Return: 0,1,-1
+ *
  */
-int print_sign(int n);
-int main(void)
-{
-  int r;
-
-  r = print_sign(98);
-  _putchar(',');
-  _putchar(' ');
-  _putchar(r + '0');
-  _putchar('\n');
-  r = print_sign(0);
-  _putchar(',');
-  _putchar(' ');
-  _putchar(r + '0');
-  _putchar('\n');
-  r = print_sign(0xff);
-  _putchar(',');
-  _putchar(' ');
-  _putchar(r + '0');
-  _putchar('\n');
-  r = print_sign(-1);
-  _putchar(',');
-  _putchar(' ');
-  _putchar(r + '0');
-  _putchar('\n');
-  return (0);
-}
 int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else
+		return (-1);
+}
