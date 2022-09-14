89ad34d2b486a763dfe91e702cd810cbaae8741a
enock-jufri <enockjufri@gmail.com>
2022-09-14 04:51:42 -0700

    update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,47 +7,21 @@
  * Return: 0,1,-1
  *
  */
-int main(void)
-{
-  int r;
-
-  r = print_sign(98);
-  _putchar(',');
-  _putchar(' ');
-  _putchar(r + '0');
-  _putchar('\n');
-  r = print_sign(0);
-  _putchar(',');
-  _putchar(' ');
-  _putchar(r + '0');
-  _putchar('\n');
-  r = print_sign(0xff);
-  _putchar(',');
-  _putchar(' ');
-  _putchar(r + '0');
-  _putchar('\n');
-  r = print_sign(-1);
-  _putchar(',');
-  _putchar(' ');
-  _putchar(r + '0');
-  _putchar('\n');
-  return (0);
-}
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar(45);
+		_putchar(45);
 		return (1);
 	}
 	else if (n < 0)
 	{
-		putchar(45);
+		_putchar(45);
 		return (-1);
 	}
 	else
 	{
-		putchar(48);
+		_putchar(48);
 		return (0);
 	}
 	putchar('\n');
