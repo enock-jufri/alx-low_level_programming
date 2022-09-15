338e34de3675f5a4ed7d8b0d09afbc4e34f8a590
enock-jufri <enockjufri@gmail.com>
2022-09-15 12:58:42 -0700

    update

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - 
+ *
+ * Return:-
+ */
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)
+	{
+		putchar(i);
+	}*/
+
+	printf("infinite looop avoided! \\o/\n");
+
+	return (0);
+}
