f8d0738f496b409496afcbc8a228dd7be511a951
enock-jufri <enockjufri@gmail.com>
2022-09-15 12:45:44 -0700

    update

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - tests function prints if integer postive or negative
+ *
+ * Return: 0
+ */
+int main()
+{
+	int i;
+
+	i = 0;
+	postive_or_negative(i);
+
+	return (0);
+}
