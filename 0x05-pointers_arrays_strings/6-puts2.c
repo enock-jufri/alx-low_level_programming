81df37fc63559fe9f29a065d46afa1e8d091dde9
enock-jufri <enockjufri@gmail.com>
2022-09-19 09:08:16 -0700

    update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,14 +6,25 @@
  *
  * Return:0
  */
-void puts2(char *s)
+void puts2(char *str)
 {
 	int l = 0;
+	char *y = *str;
+	int len = 0;
+	int z = 0;
 
-	while (*(s + l) != '\0')
+	while (*y != '\0')
 	{
-		putchar(s[l]);
+		y++;
+		len++;
+	}
+	l = len - 1;
+	for (z = 0; z <= l; z++)
+	{
+		if (z % 2 == 0)
+		{
+			putchar(str[z])
+		}
 		putchar('\n');
-		l++;
 	}
 }
