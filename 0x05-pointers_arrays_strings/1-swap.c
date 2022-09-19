5ef37d127a8e577971fe6d8c475e39f7fec04764
enock-jufri <enockjufri@gmail.com>
2022-09-18 21:25:36 -0700

    update

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * swap_int - swaps ints for two numbers
+ *
+ * @a: parameter a
+ * @b: parameter b
+ *
+ * Return:0
+ */
+void swaps_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
