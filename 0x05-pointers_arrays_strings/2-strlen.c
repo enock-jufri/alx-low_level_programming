c300b62a3a5d571b0a03a90e59bc64ed5ab3dc02
enock-jufri <enockjufri@gmail.com>
2022-09-18 21:40:29 -0700

    update

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * int_strlen - length of string
+ *
+ * @s: string parameter
+ *
+ * Return:0
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while(s != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
