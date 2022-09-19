62acaacd21f7a31fb3e1cc7ad7214ecacb1ab568
enock-jufri <enockjufri@gmail.com>
2022-09-19 08:38:50 -0700

    update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints string in reverse
+ *
+ * @s: string parameter
+ *
+ * Return:0
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != 0; len++)
+	{
+	}
+	for (len = len -1; len >= 0; len--)
+	{
+		putchar(s[len]);
+	}
+	putchar('\n');
+}
